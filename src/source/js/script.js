bd09c7d7a8aa9953da86ea5f601e44ad31092d79
Vitaliy Parasochkin <parasochkinvitaliy@gmail.com>
2023-03-09 14:14:50 +0200

    deleted page src

--- src/source/js/script.js
+++
@@ -1,9 +0,0 @@
-//choose theme for site
-
-//burger
-const burgerIcon = document.querySelector(".header-icon");
-burgerIcon.addEventListener("click", () => {
-    burgerIcon.classList.toggle("active");
-    document.querySelector(".header-menu").classList.toggle("active");
-    document.querySelector("body").classList.toggle("lock");
-});
